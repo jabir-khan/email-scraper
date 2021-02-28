bcb626f05f6c24c01adb506d79aa8bc3a67a411c
jabir-khan <jabirkhan710@gmail.com>
2021-02-28 23:03:52 +0545

    second commit

--- app.py
+++ app.py
@@ -38,67 +38,67 @@ def scrapper(url):
                               executable_path=chrome_driver)
     '''----------------------------end option settings--------------------'''
 
-    # driver.get(url)
-    # time.sleep(2)
-    # # search_box = driver.find_element_by_id("findTypeaheadInput")   
-    # # ''' pass search keyword to search field '''
-    # # search_box.send_keys(search_keyword)
+    driver.get(url)
+    time.sleep(2)
+    # search_box = driver.find_element_by_id("findTypeaheadInput")   
+    # ''' pass search keyword to search field '''
+    # search_box.send_keys(search_keyword)
 
-    # # location = driver.find_element_by_id("nearTypeaheadInput")
-    # # location.send_keys(Keys.CONTROL, 'a')
-    # # location.send_keys(Keys.CONTROL, 'x')
-    # # ''' pass location to search field '''
-    # # location.send_keys("San Francisco, CA")
+    # location = driver.find_element_by_id("nearTypeaheadInput")
+    # location.send_keys(Keys.CONTROL, 'a')
+    # location.send_keys(Keys.CONTROL, 'x')
+    # ''' pass location to search field '''
+    # location.send_keys("San Francisco, CA")
     
         
-    # # search_box.send_keys(Keys.ENTER)
-    # # time.sleep(1)
+    # search_box.send_keys(Keys.ENTER)
+    # time.sleep(1)
 
-    # ''' get all organization details '''
-    # html = driver.page_source
-    # soup = BeautifulSoup(html, 'html.parser')
-    # org_names = soup.find_all('a', {'class': 'Name__Link-sc-12ndgzr-1 duvGnB'},href=True)
+    ''' get all organization details '''
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    org_names = soup.find_all('a', {'class': 'Name__Link-sc-12ndgzr-1 duvGnB'},href=True)
 
           
 
-    # phones = soup.find_all('p', {'class': 'Phone-sc-1p1gq9f-0'})
-    # address = soup.find_all('p', {'class':'Address-sc-9uj8rv-0'})
+    phones = soup.find_all('p', {'class': 'Phone-sc-1p1gq9f-0'})
+    address = soup.find_all('p', {'class':'Address-sc-9uj8rv-0'})
   
-    # for x in range(len(org_names)):        
-    #     bbb_org_name.append(org_names[x].text)
-    #     # bbb_phone.append(phones[phone].a.text)
-    #     ''' extract all the business links from org_names '''
-    #     link = org_names[x]['href']
-    #     # print(org_names[x]['href'])
+    for x in range(len(org_names)):        
+        bbb_org_name.append(org_names[x].text)
+        # bbb_phone.append(phones[phone].a.text)
+        ''' extract all the business links from org_names '''
+        link = org_names[x]['href']
+        # print(org_names[x]['href'])
 
-    #     ''' open links one by one and grab the website url '''
-    #     driver.get(link)
-    #     time.sleep(3)
-    #     html = driver.page_source
-    #     soup = BeautifulSoup(html, 'html.parser')
-    #     web_url = soup.find('a', {'class': 'styles__LinkStyled-sc-13wt55m-0 bCFCsn dtm-url'})
+        ''' open links one by one and grab the website url '''
+        driver.get(link)
+        time.sleep(3)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        web_url = soup.find('a', {'class': 'styles__LinkStyled-sc-13wt55m-0 bCFCsn dtm-url'})
         
-    #     if web_url is not None:
-    #         print(web_url.text) 
-    #         bbb_website.append(web_url.text)
+        if web_url is not None:
+            print(web_url.text) 
+            bbb_website.append(web_url.text)
             
-    #     def Check_duplicate(x):
-    #         _size = len(x) 
-    #         new_url_list = [] 
-    #         for i in range(_size): 
-    #             k = i + 1
-    #             for j in range(k, _size): 
-    #                 if x[i] == x[j] and x[i] not in new_url_list: 
-    #                     new_url_list.append(x[i]) 
-    #         return new_url_list or "Searching url"
-    #     # This code is contributed by Sandeep_anand in GeeksforGeeks
-    #     print (Check_duplicate(bbb_website))
-    #     bbb_website_links = Check_duplicate(bbb_website)
+        def Check_duplicate(x):
+            _size = len(x) 
+            new_url_list = [] 
+            for i in range(_size): 
+                k = i + 1
+                for j in range(k, _size): 
+                    if x[i] == x[j] and x[i] not in new_url_list: 
+                        new_url_list.append(x[i]) 
+            return new_url_list or "Searching url"
+        # This code is contributed by Sandeep_anand in GeeksforGeeks
+        print (Check_duplicate(bbb_website))
+        bbb_website_links = Check_duplicate(bbb_website)
 
         
     print("All links are collected now opening the links...")
 
-    bbb_website_links = ['http://elusiktax.com', 'http://www.oldbustavern.com', 'http://www.crimsoninsurance.com', 'http://ggba.com/', 'http://www.lcbusinesssystems.com', 'https://simplybusinessclass.com/businessclassbbb', 'http://www.ruabusiness.com']
+    
 
     
     for links in range(len(bbb_website_links)):
@@ -164,7 +164,7 @@ print("[+] Location is set to San Francisco, CA")
 
 # url = "https://www.bbb.org"
 
-for i in range(0,10):
+for i in range(6,10):
     page_no = i+1
     url = "https://www.bbb.org/search?find_country=USA&find_loc=San%20Francisco%2C%20CA&find_text="+search_keyword+"&page="+str(page_no)
     # print(driver.current_url)
